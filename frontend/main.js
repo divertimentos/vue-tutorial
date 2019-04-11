68c45d3ed8c238e2b48019f882a9773620637b98
guiemi <guiemi.teixeira@gmail.com>
2019-04-10 21:23:40 -0300

    Cria uma página onde um produto e uma imagem são mostrados com o Vue

---
+++ frontend/main.js
@@ -0,0 +1,9 @@
+var app  = new Vue({
+    el: '#app', 
+    data: {
+        product: 'Socks',
+        description: 'White socks with a heart-shaped leaf inside it!',
+        image: 'vmSocks-green-onWhite.jpg',
+    }
+})
+
